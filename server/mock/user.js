9c21785dac3abc2e43f7c6e24ab93d7de214ca3c
mastersam <abadasamuelosp@gmail.com>
2019-11-14 04:32:16 -0800

    completed api

---
+++ server/mock/user.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    id: 5,
+    firstName: 'abada',
+    lastName: 'samuel',
+    email: 'abadasamuelosp@gmail.com',
+    password: '$2b$08$q7.kLHnw35NliZ46W0RXneQmhBKI5Z978iq/Cw99eifImj9N3iHl6',
+    textPassword: 'password',
+    gender: 'male',
+    jobRole: 'developer',
+    department: 'Software developer',
+    address: 'Lagos',
+  },
+  {
+    firstName: 'samuel',
+    lastName: 'abada',
+    email: 'abadasosp@gmail.com',
+    password: 'password',
+    gender: 'male',
+    jobRole: 'developer',
+    department: 'Software developer',
+    address: 'lagos',
+  },
+];
+
+export default users;
